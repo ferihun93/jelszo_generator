16bb6da9ec31a308de264a1b21f5821d72bbb0ae
ferihun93 <ferenc.olah1993@gmail.com>
2024-12-30 12:56:04 +0100

    elso_verzio

--- main.js
+++ main.js
@@ -1,45 +1,65 @@
-let kertkarakterSzam = document.getElementById("karakter-szam");
-let kiirtJelszo = document.getElementById("jelszo");
-// let tesztSzamjegy = Number(
-//   prompt("Írd ide hogy hány karakteres jelszót szeretnél!")
-// );
+import { betuk } from "./karakterek.js";
+import { specialisKarakterek } from "./karakterek.js";
+
+const kertkarakterSzam = document.getElementById("karakter-szam");
+const kiirtJelszo = document.getElementById("jelszo");
+const betukSzama = document.getElementById("betuk-szama");
+const specialKarakterekSzama = document.getElementById("speckarakterek-szama");
+
 let kezdoKarakterekSzama = 0;
+let kezdoBetukSzama = 0;
+let kezdoSpeckarakterekSzama = 0;
 let jelszoKarakterek = [];
-let randomSzam = 0;
-let ujRandomSzam = 0;
 
 const randomSzamGeneralo = function () {
   while (kezdoKarakterekSzama < kertkarakterSzam.value) {
-    randomSzam = Math.floor(Math.random() * 9 + 1);
+    const randomSzam = Math.floor(Math.random() * 9 + 1);
+    jelszoKarakterek.push(randomSzam);
     kezdoKarakterekSzama++;
-    tombFeltoltes();
   }
-  // tombAtvizsgalo();
+  tombFeltoltes();
 };
 
-const ujRandomSzamGeneralas = function () {
-  Math.floor(Math.random() * 9 + 1);
+const randomBetukGeneralo = function () {
+  while (kezdoBetukSzama < betukSzama.value) {
+    const randomIndex = Math.floor(Math.random() * betuk.length);
+    const randomBetuk = betuk[randomIndex];
+    jelszoKarakterek.push(randomBetuk);
+    kezdoBetukSzama++;
+  }
 };
 
-// const tombAtvizsgalo = function () {
-//   for (let szam of jelszoKarakterek) {
-//     if (szam === randomSzam) {
-//       randomSzam = ujRandomSzamGeneralas();
-//       jelszoKarakterek.push(randomSzam);
-//     } else {
-//       randomSzamGeneralo();
-//     }
-//   }
-// };
+const randomSpecialisKarakterGeneralo = function () {
+  while (kezdoSpeckarakterekSzama < specialKarakterekSzama.value) {
+    const randomIndex = Math.floor(Math.random() * specialisKarakterek.length);
+    const randomSpecBetuk = specialisKarakterek[randomIndex];
+    jelszoKarakterek.push(randomSpecBetuk);
+    kezdoSpeckarakterekSzama++;
+  }
+};
+
+const jelszoRandomizalo = function () {
+  for (let i = jelszoKarakterek.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [jelszoKarakterek[i], jelszoKarakterek[randomIndex]] = [
+      jelszoKarakterek[randomIndex],
+      jelszoKarakterek[i],
+    ];
+  }
+};
 
 const tombFeltoltes = function () {
-  jelszoKarakterek.push(randomSzam);
+  randomBetukGeneralo();
+  randomSpecialisKarakterGeneralo();
+  jelszoRandomizalo();
   let ujJelszo = jelszoKarakterek.join("");
   kiirtJelszo.value = ujJelszo;
 };
 
 const reset = function () {
   kezdoKarakterekSzama = 0;
+  kezdoBetukSzama = 0;
+  kezdoSpeckarakterekSzama = 0;
   jelszoKarakterek = [];
   kiirtJelszo.value = "";
 };
